d3abe463454786a6fee11dda49b92018e109671d
E.G. ismaail <contact@ismaail.com>
2016-04-16 19:58:05 +0100

    Update LanguageRepositoryTest

--- tests/AppBundle/Repository/LanguageRepositoryTest.php
+++ tests/AppBundle/Repository/LanguageRepositoryTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\AppBundle\Repository;
 
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use AppBundle\Entity\Language;
 use Tests\DoctrineTestCase;
 
 /**
@@ -25,6 +28,24 @@ class LanguageRepositoryTest extends DoctrineTestCase
     }
 
     /**
+     * Create & persist Language entity in the database.
+     *
+     * @param array $data
+     */
+    private function createLanguage(array $data)
+    {
+        $language = new Language();
+        $language
+            ->setCode($data['code'])
+            ->setName($data['name'])
+            ->setIsDefault($data['isDefault'])
+        ;
+
+        $this->getEntityManager()->persist($language);
+        $this->getEntityManager()->flush();
+    }
+
+    /**
      * @test
      */
     public function languages_table_is_initialy_empty()
@@ -32,5 +53,65 @@ class LanguageRepositoryTest extends DoctrineTestCase
         $languages = $this->languageRepository->findAll();
 
         $this->assertCount(0, $languages);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_exception_if_no_default_language_is_found()
+    {
+        $this->expectException(NoResultException::class);
+
+        $this->languageRepository->findDefault();
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_exception_if_more_than_one_default_language_is_found()
+    {
+        $this->createLanguage(['code' => 'en', 'name' => 'English', 'isDefault' => true]);
+        $this->createLanguage(['code' => 'fr', 'name' => 'Français', 'isDefault' => true]);
+
+        $this->expectException(NonUniqueResultException::class);
+
+        $this->languageRepository->findDefault();
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_the_correct_default_language()
+    {
+        $this->createLanguage(['code' => 'en', 'name' => 'English', 'isDefault' => false]);
+        $this->createLanguage(['code' => 'fr', 'name' => 'Français', 'isDefault' => true]);
+        $this->createLanguage(['code' => 'de', 'name' => 'Deutsch', 'isDefault' => false]);
+
+        $language = $this->languageRepository->findDefault();
+
+        $this->assertInstanceOf(Language::class, $language);
+        $this->assertSame('fr', $language->getCode());
+        $this->assertSame('Français', $language->getName());
+        $this->assertSame(true, $language->getIsDefault());
+    }
+
+    /**
+     * @test
+     */
+    public function it_sets_language_isDefault_value_to_false()
+    {
+        $language = new Language();
+        $language
+            ->setCode('en')
+            ->setName('English')
+        ;
+
+        $this->getEntityManager()->persist($language);
+        $this->getEntityManager()->flush();
+
+        $languages = $this->languageRepository->findAll();
+
+        $this->assertCount(1, $languages);
+        $this->assertFalse($languages[0]->getIsDefault(), 'Language "isDefault" value is not "false"');
     }
 }
