b29eb93dcfbd2d4677f82c0da6afbdcae770a710
E.G. ismaail <contact@ismaail.com>
2016-04-16 19:57:42 +0100

    Add findDefault method in LanguageRepository

--- src/AppBundle/Repository/LanguageRepository.php
+++ src/AppBundle/Repository/LanguageRepository.php
@@ -3,13 +3,32 @@
 namespace AppBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use AppBundle\Entity\Language;
 
 /**
  * Class LanguageRepository
  * @package AppBundle\Repository
  *
- * @method \AppBundle\Entity\Language[] findAll()
+ * @method Language[] findAll()
  */
 class LanguageRepository extends EntityRepository
 {
+    /**
+     * Fidn the deault language.
+     *
+     * @return Language
+     *
+     * @throws \Doctrine\ORM\NoResultException          If not default language is found.
+     * @throws \Doctrine\ORM\NonUniqueResultException   If more than one default language is found.
+     */
+    public function findDefault()
+    {
+        $qb = $this->createQueryBuilder('l');
+        $qb
+            ->select('l')
+            ->where('l.isDefault = true')
+        ;
+
+        return $qb->getQuery()->getSingleResult();
+    }
 }
